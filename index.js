4adaaa8a886ac45708365c3c4ae00c0d76244498
cdAkash <akashyadav15032002@gmail.com>
2024-11-09 08:32:42 +0530

    FirstCommit3

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000;
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,8 +16,8 @@ app.get('/login',(req,res)=>{
   res.send('<h1>Please Login at chai aur code</h1>')
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
 
 
